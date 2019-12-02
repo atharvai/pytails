60bc7b4828a99d9c2c2968a48796db51be97d2a8
atharvai <atharva.inamdar07@gmail.com>
2019-12-02 08:02:06 +0000

    add tests and restructure dirs

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='pyTails',
+      version='1.0.0',
+      description='Python DB tailer',
+      author='Atharva Inamdar',
+      packages=['pytails'],
+     )
