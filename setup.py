92f9bf3db50581b1215aa0a1ff304ae35ea1c981
atharvai <atharva.inamdar07@gmail.com>
2020-01-04 07:53:39 +0000

    version update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pyTails',
-      version='0.0.1',
+      version='0.0.2',
       description='Python DB tailer',
       author='Atharva Inamdar',
       packages=['pytails'],
