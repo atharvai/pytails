090333089514af07cb840288a2ea6644bec14875
atharvai <atharva.inamdar07@gmail.com>
2020-01-04 07:43:11 +0000

    fix some imports and script

--- pytails/pytails.py
+++ pytails/pytails.py
@@ -6,25 +6,28 @@ import os
 import logging
 import sys
 
-from pytails.mongo import OplogClient
-from pytails.sinks import ConsoleSink, KinesisSink, FirehoseSink
+from pytails.mongo.oplog_client import OplogClient
+from pytails.sinks import ConsoleSink
+from pytails.sinks.kinesis import KinesisSink
+from pytails.sinks.firehose import FirehoseSink
 from pytails.state.ddb_store import DynamoDbStore
 
 logger = logging.getLogger(__name__)
 
-if __name__ == '__main__':
+
+def main():
     logging.basicConfig(level=logging.INFO, stream=sys.stdout,
                         format='%(asctime)s - %(levelname)s - %(name)s - %(Func)s %(Op)s %(message)s')
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--mongo-host', type=str, default=os.environ.get('MONGO_HOST', None),
-                        help='MongoDB replica set host to tail')
+                        help='MongoDB replica set host to tail', required=True)
     parser.add_argument('--mongo-port', type=int, default=os.environ.get('MONGO_PORT', 27017),
-                        help='MongoDB replica set port')
+                        help='MongoDB replica set port', required=True)
     parser.add_argument('--tail-id', type=str, default=os.environ.get('TAIL_ID', None),
-                        help='Unique identifier for tail. usually short name for Mongodb cluster')
+                        help='Unique identifier for tail. usually short name for Mongodb cluster', required=True)
     parser.add_argument('--replica-set', type=str, default=os.environ.get('REPLICA_SET', None),
-                        help='Mongodb Replica set name')
+                        help='Mongodb Replica set name', required=True)
     parser.add_argument('--kinesis-data-sink', type=str, default=os.environ.get('KINESIS_DATA_SINK', None),
                         help='Kinesis Data Stream Name. Not ARN')
     parser.add_argument('--firehose-data-sink', type=str, default=os.environ.get('FIREHOSE_DATA_SINK', None),
@@ -64,3 +67,6 @@ if __name__ == '__main__':
         if args.firehose_data_sink:
             client.register_data_sink(FirehoseSink(client.identifier, args.firehose_data_sink))
         client.tail()
+
+
+main()
